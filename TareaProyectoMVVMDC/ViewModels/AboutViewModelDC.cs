f574c1e691badb535d2717efede176ca15ab407a
DiegoCorrea07 <diego.correa.cumbicus07@outlook.com>
2024-07-09 22:13:54 -0500

    Cambio de colores y agregar un scroll a las vistas, para mejorar la presentación.

--- TareaProyectoMVVMDC/ViewModels/AboutViewModelDC.cs
+++ TareaProyectoMVVMDC/ViewModels/AboutViewModelDC.cs
@@ -13,7 +13,12 @@ namespace TareaproyectoMVVMDC.ViewModels
         public string Title => AppInfo.Name;
         public string Version => AppInfo.VersionString;
         public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public string Message => "¡Hola! Soy un apasionado estudiante en Ingeniería de " +
+            "Software que disfruta creando aplicaciones innovadoras y útiles. Con experiencia en " +
+            "diversas tecnologías de desarrollo, estoy constantemente aprendiendo y " +
+            "mejorando mis habilidades para ofrecer soluciones de alta calidad. Fuera " +
+            "del mundo del desarrollo, me encanta explorar nuevas tecnologías, leer " +
+            "libros y disfrutar de actividades al aire libre como jugar fútbol.";
         public ICommand ShowMoreInfoCommand { get; }
 
         public AboutViewModelDC()
